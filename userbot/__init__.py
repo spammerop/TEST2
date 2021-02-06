36b25bad269cf0caa189fc31658afc4c089c194f
H1M4N5HU0P <75165252+H1M4N5HU0P@users.noreply.github.com>
2021-02-06 19:03:56 +0530

    Add files via upload

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -150,7 +150,7 @@ except:
     # Upstream Repo
     UPSTREAM_REPO_URL = os.environ.get(
     "UPSTREAM_REPO_URL",
-    "https://github.com/HellBoy-OP/HellBot.git")
+    "https://www.github.com/SPAMMEROP/TEST2.git")
 
     # Last.fm Module
     BIO_PREFIX = os.environ.get("BIO_PREFIX", None)
